c261c7f9b87bfa14c77b62f0ad5b67047e03b9bf
Krystian Kupiec <k.kupiec@koiasoft.no>
2023-06-06 01:52:36 +0200

    build: update serve and add repo instructions

--- apps/web-project-name/src/main.tsx
+++ apps/web-project-name/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
-import { App } from './features/app';
+import { App } from './app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
